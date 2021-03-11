c4d4ebdbd6910e562f09fbc15b4c1bb4622e6f27
Susana <susana.m.vik@gmail.com>
2021-03-11 09:41:10 -0500

    commit tree 50944fb5b8bae97e75d54043873185d189789a10

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,18 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+    attr_reader :limit, :multiples
+
+    def initialize(limit)
+        @limit = limit - 1
+        @multiples = collect_multiples
+    end
+
+    def collect_multiples
+        (1...limit).to_a.reject {|num| num unless num % 3 == 0 || num % 5 == 0}
+    end
+                
+    def sum_multiples
+       multiples.inject(:+)
+    end
+end
